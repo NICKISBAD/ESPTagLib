2023894431711b50553d07bdb8e5d0eb4be3ce53
Nick <107813557+NICKISBAD@users.noreply.github.com>
2023-08-10 09:11:42 -0700

    Update ESPLib.lua

--- ESPLib.lua
+++ ESPLib.lua
@@ -5,10 +5,11 @@ function ESPLib:CreateESPTag(params)
     local player = game.Players.LocalPlayer
     local camera = game:GetService("Workspace").CurrentCamera
 
-    local Name = params[1]
-    local Part = params[2]
-    local Size = params[3]
-    local TextColor = params[4]
+    local Text = params.Text
+    local Part = params.Part
+    local TextSize = params.TextSize
+    local TextColor = params.TextColor
+    local BoxColor = params.BoxColor
 
     local esp = Instance.new("BillboardGui")
     esp.Name = "esp"
@@ -26,15 +27,25 @@ function ESPLib:CreateESPTag(params)
     esplabelfr.BackgroundTransparency = 1
     esplabelfr.TextStrokeTransparency = 0
     esplabelfr.TextStrokeColor3 = Color3.new(0, 0, 0)
-    esplabelfr.TextSize = Size
+    esplabelfr.TextSize = TextSize
     esplabelfr.TextScaled = false
     esplabelfr.Parent = esp
+
+    local box = Instance.new("BoxHandleAdornment")
+    box.Name = "box"
+    box.Size = Part.Size + Vector3.new(0.5, 0.5, 0.5)
+    box.Adornee = Part
+    box.AlwaysOnTop = true
+    box.Transparency = 0.6
+    box.Color3 = BoxColor or Color3.new(0, 0, 255)
+    box.ZIndex = 0
+    box.Parent = Part
 
     local function updateesplabelfr()
         local playerPosition = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
         if playerPosition then
             local distance = (playerPosition.Position - Part.Position).Magnitude
-            esplabelfr.Text = string.format(Name .. ": %.2f", distance)
+            esplabelfr.Text = string.format(Text .. ": %.2f", distance)
 
             local headPosition = Part.Position + Vector3.new(0, Part.Size.Y / 2, 0)
             local screenPosition, onScreen = camera:WorldToScreenPoint(headPosition)
@@ -42,13 +53,44 @@ function ESPLib:CreateESPTag(params)
             if onScreen or playerPosition.Position.Y > Part.Position.Y then
                 esp.Adornee = Part
                 esp.Enabled = true
+                box.Adornee = Part
+                box.Visible = true
             else
                 esp.Enabled = false
+                box.Visible = false
             end
         else
             esp.Enabled = false
+            box.Visible = false
         end
     end
 
     RunService.RenderStepped:Connect(updateesplabelfr)
 end
+
+function ESPLib:PlayerESP()
+    for i, player in pairs(game.Players:GetChildren()) do
+        if player:IsA("Player") and player.Name ~= game.Players.LocalPlayer.Name then
+            ESPLib:CreateESPTag({
+                Text = player.DisplayName .. "/" .. player.Name,
+                Part = player.Character.UpperTorso or player.Character.Torso,
+                TextSize = 10,
+                TextColor = Color3.new(255, 255, 255),
+                BoxColor = Color3.new(255, 255, 255)
+            })
+        end
+    end
+end
+
+--[[ 
+    Func Uses = ({
+        ESPLib:PlayerESP() -- Built in Player ESP
+        ESPLib:CreateESPTag({ -- Create an ESP Tag
+            Text = "Sample",
+            Part = game.Workspace.Part,
+            TextSize = 20,
+            TextColor = Color3.new(255,255,255),
+            BoxColor = Color3.new(255,255,255)
+        })
+    }
+]]
