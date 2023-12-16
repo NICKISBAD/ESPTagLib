070392aca4f8c7340d123d4587bb7d78fb80f506
Nick <107813557+NICKISBAD@users.noreply.github.com>
2023-12-16 19:30:26 +0500

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -7,55 +7,82 @@ if _G.DestroyESP then
     end
 end
 
--- Box
-if _G.Settings.Boxes.Enabled then
-    local box = Instance.new("BoxHandleAdornment")
-    box.Size = _G.Settings.Part.Size + Vector3.new(0, 0.5, 0)
-    box.Color3 = _G.Settings.Boxes.Color
-    box.Transparency = 0.5
-    box.AlwaysOnTop = true
-    box.Adornee = _G.Settings.Part
-    box.Parent = _G.MasterSwitch and _G.Settings.Part or nil
-end
+-- Check if the part has a Humanoid
+local humanoid = _G.Settings.Part:FindFirstChildOfClass("Humanoid")
 
--- ... (Same code for other ESP elements)
+if humanoid then
+    -- Box
+    if _G.Settings.Boxes.Enabled then
+        local box = Instance.new("BoxHandleAdornment")
+        box.Size = _G.Settings.Part.Size + Vector3.new(0, 0.5, 0)
+        box.Color3 = _G.Settings.Boxes.Color
+        box.Transparency = 0.5
+        box.AlwaysOnTop = true
+        box.Adornee = _G.Settings.Part
+        box.Parent = _G.MasterSwitch and _G.Settings.Part or nil
+    end
 
--- Walkspeed
-if _G.Settings.Walkspeed.Enabled then
-    local walkspeedLabel = Instance.new("TextLabel")
-    walkspeedLabel.Text = "Walkspeed: " .. math.floor(game.Players.LocalPlayer.Character.Humanoid.WalkSpeed)
-    walkspeedLabel.TextColor3 = _G.Settings.Walkspeed.Color
-    walkspeedLabel.TextStrokeTransparency = 0
-    walkspeedLabel.TextStrokeColor3 = _G.Settings.BoxOutlineColor
-    walkspeedLabel.Position = UDim2.new(0, 0, 0, 90) -- Adjust position as needed
-    walkspeedLabel.Parent = _G.MasterSwitch and _G.Settings.Part or nil
-end
+    -- 2D Outline (Outer)
+    if _G.Settings._2D_Outline.Enabled then
+        local outlineOuter = Instance.new("BoxHandleAdornment")
+        outlineOuter.Size = _G.Settings.Part.Size + Vector3.new(0.5, 0.5, 0.5)
+        outlineOuter.Color3 = _G.Settings._2D_Outline.Color
+        outlineOuter.Transparency = 0.5
+        outlineOuter.AlwaysOnTop = true
+        outlineOuter.Adornee = _G.Settings.Part
+        outlineOuter.Parent = _G.MasterSwitch and _G.Settings.Part or nil
+    end
 
--- ... (Same code for other ESP elements)
+    -- 2D Outline (Inner - Box)
+    if _G.Settings._2D_Outline.Enabled then
+        local outlineInnerBox = Instance.new("BoxHandleAdornment")
+        outlineInnerBox.Size = _G.Settings.Part.Size + Vector3.new(0.3, 0.3, 0.3)
+        outlineInnerBox.Color3 = Color3.new(0.7, 1, 0.7) -- Light-ish green
+        outlineInnerBox.Transparency = 0.5
+        outlineInnerBox.AlwaysOnTop = true
+        outlineInnerBox.Adornee = _G.Settings.Part
+        outlineInnerBox.Parent = _G.MasterSwitch and _G.Settings.Part or nil
+    end
 
--- Update labels on a timer
-while wait(1) do
+    -- Walkspeed
     if _G.Settings.Walkspeed.Enabled then
-        walkspeedLabel.Text = "Walkspeed: " .. math.floor(game.Players.LocalPlayer.Character.Humanoid.WalkSpeed)
+        local walkspeedLabel = Instance.new("TextLabel")
+        walkspeedLabel.Text = "Walkspeed: " .. math.floor(humanoid.WalkSpeed)
+        walkspeedLabel.TextColor3 = _G.Settings.Walkspeed.Color
+        walkspeedLabel.TextStrokeTransparency = 0
+        walkspeedLabel.TextStrokeColor3 = _G.Settings.BoxOutlineColor
+        walkspeedLabel.Position = UDim2.new(0, 0, 0, 90) -- Adjust position as needed
+        walkspeedLabel.Parent = _G.MasterSwitch and _G.Settings.Part or nil
     end
 
-    if _G.Settings.Health.Enabled then
-        local health = math.floor(game.Players.LocalPlayer.Character.Humanoid.Health)
-        healthLabel.Text = "Health: " .. health .. "%"
+    -- Update labels on a timer
+    spawn(function()
+        while wait(1) do
+            if _G.Settings.Walkspeed.Enabled then
+                walkspeedLabel.Text = "Walkspeed: " .. math.floor(humanoid.WalkSpeed)
+            end
 
-        -- Change health label color based on health status
-        if health > 75 then
-            healthLabel.TextColor3 = _G.Settings.Health.HighColor
-        elseif health > 25 then
-            healthLabel.TextColor3 = _G.Settings.Health.Color
-        else
-            healthLabel.TextColor3 = _G.Settings.Health.LowColor
+            if _G.Settings.Health.Enabled then
+                local health = math.floor(humanoid.Health)
+                healthLabel.Text = "Health: " .. health .. "%"
+
+                -- Change health label color based on health status
+                if health > 75 then
+                    healthLabel.TextColor3 = _G.Settings.Health.HighColor
+                elseif health > 25 then
+                    healthLabel.TextColor3 = _G.Settings.Health.Color
+                else
+                    healthLabel.TextColor3 = _G.Settings.Health.LowColor
+                end
+            end
+
+            if _G.Settings.Jumppower.Enabled then
+                jumppowerLabel.Text = "Jumppower: " .. math.floor(humanoid.JumpPower)
+            end
+
+            -- Add other stat updates here
         end
-    end
-
-    if _G.Settings.Jumppower.Enabled then
-        jumppowerLabel.Text = "Jumppower: " .. math.floor(game.Players.LocalPlayer.Character.Humanoid.JumpPower)
-    end
-
-    -- Add other stat updates here
+    end)
+else
+    warn("The part does not have a Humanoid. Health and stats won't be displayed.")
 end
